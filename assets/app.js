2823ccfbd2511082669532dfc9003832268ce8df
Harika-BRS <153584946+Harika-BRS@users.noreply.github.com>
2024-03-20 14:29:04 +0530

    files uploaded

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,42 @@
 document.getElementById("submit").addEventListener("click", function() {
-    const inputString = document.getElementById("intervalsInput").value.trim();
-    const inputArray = inputString.split(",").map(Number); // Convert input string to an array of numbers
+  processInput();
+});
 
-    // Call the pivotIndex function with inputArray
-    const result = pivotIndex(inputArray);
+document.getElementById("intervalsInput").addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+      processInput();
+  }
+});
 
-    // Display the result in the outputDiv
-    document.getElementById("output").textContent = result.toString();
-  });
+function processInput() {
+  const inputString = document.getElementById("intervalsInput").value.trim();
+  const inputArray = inputString.split(",").map(Number); // Convert input string to an array of numbers
 
-  const pivotIndex = (nums) => {
-    let rightSum = 0;
-    let leftSum = 0;
+  // Call the pivotIndex function with inputArray
+  const result = pivotIndex(inputArray);
 
-    nums.forEach((v) => (rightSum += v));
+  // Display the result in the outputDiv
+  document.getElementById("output").textContent = result.toString();
+}
 
-    for (var i = 0; i <= nums.length - 1; i++) {
-        let curr = nums[i];
-        rightSum -= curr;
-        if (leftSum === rightSum) return i;
-        leftSum += curr;
-    }
+const pivotIndex = (nums) => {
+  let rightSum = 0;
+  let leftSum = 0;
 
-    return -1;
-  };
+  nums.forEach((v) => (rightSum += v));
+
+  for (var i = 0; i <= nums.length - 1; i++) {
+      let curr = nums[i];
+      rightSum -= curr;
+      if (leftSum === rightSum) return i;
+      leftSum += curr;
+  }
+
+  return -1;
+};
+
 // Add this script to clear the input field on page load
 document.addEventListener('DOMContentLoaded', function() {
-    var intervalsInput = document.getElementById('intervalsInput');
-    intervalsInput.value = ''; // Set the input value to an empty string
-  });
+  var intervalsInput = document.getElementById('intervalsInput');
+  intervalsInput.value = ''; // Set the input value to an empty string
+});
